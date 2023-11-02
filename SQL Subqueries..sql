63f96f36d009116bf1df3d9e9a7a7688c552bd8e
rodasbilro94 <143207859+rodasbilro94@users.noreply.github.com>
2023-11-02 17:25:44 +0000

    Add files via upload

---
+++ SQL Subqueries..sql
@@ -0,0 +1,45 @@
+-- 1
+SELECT COUNT(inventory.inventory_id) AS Copies
+FROM film
+JOIN inventory ON film.film_id = inventory.film_id
+WHERE film.title = 'Hunchback Impossible';
+
+-- 2
+SELECT title FROM film
+WHERE length > (
+    SELECT AVG(length) FROM film
+);
+
+-- 3
+SELECT a.first_name, a.last_name
+FROM actor a
+JOIN film_actor fa ON a.actor_id = fa.actor_id
+JOIN film f ON fa.film_id = f.film_id
+WHERE f.title = 'Alone Trip';
+
+-- 4
+SELECT f.title
+FROM film f
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON fc.category_id = c.category_id
+WHERE c.name = 'Family';
+
+-- 5
+SELECT c.first_name, c.last_name, c.email
+FROM customer c
+JOIN address a ON c.address_id = a.address_id
+JOIN city ci ON a.city_id = ci.city_id
+JOIN country co ON ci.country_id = co.country_id
+WHERE co.country = 'Canada';
+
+-- 6
+SELECT f.title
+FROM film f
+JOIN film_actor fa ON f.film_id = fa.film_id
+WHERE fa.actor_id = (
+    SELECT actor_id
+    FROM film_actor
+    GROUP BY actor_id
+    ORDER BY COUNT(film_id) DESC
+    LIMIT 1
+);
